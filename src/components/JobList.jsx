0ecca5c0e02d96da20e8fa13813d0972ccc95f9f
San <san841489@gmail.com>
2024-10-21 14:35:27 +0800

    adding state and react-icons to jobListing

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import { useState } from "react";
+import { FaMapMarker } from "react-icons/fa";
 
 const JobList = ({ jobs }) => {
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
+  let description = jobs.description;
+
+  if (!showFullDescription) {
+    description = description.substring(0, 90) + "...";
+  }
+
   return (
     <div>
       <div className="bg-white rounded-xl shadow-md relative">
@@ -10,7 +19,13 @@ const JobList = ({ jobs }) => {
             <h3 className="text-xl font-bold">{jobs.title}</h3>
           </div>
 
-          <div className="mb-5">{jobs.description}</div>
+          <div className="mb-5">{description}</div>
+          <button
+            onClick={() => setShowFullDescription((prevState) => !prevState)}
+            className="text-indigo-500 mb-5"
+          >
+            {showFullDescription ? "Less" : "More"}
+          </button>
 
           <h3 className="text-indigo-500 mb-2">{jobs.salary} / Year</h3>
 
@@ -18,7 +33,7 @@ const JobList = ({ jobs }) => {
 
           <div className="flex flex-col lg:flex-row justify-between mb-4">
             <div className="text-orange-700 mb-3">
-              <i className="fa-solid fa-location-dot text-lg"></i>
+              <FaMapMarker className="inline text-lg mb-1 mr-1" />
               {jobs.location}
             </div>
             <a
